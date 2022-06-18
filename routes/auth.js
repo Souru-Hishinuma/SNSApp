e329c7a33357925ebd6a22f2e47e955390ca82ce
Souru-Hishinuma <soul.engineer@outlook.jp>
2022-06-18 16:40:27 +0900

    login and update

--- routes/auth.js
+++ routes/auth.js
@@ -16,5 +16,19 @@ router.post("/register", async (req, res) => {
     }
 })
 
+//ログイン
+router.post('/login', async (req, res) => {
+    try{
+        const user = await User.findOne({email: req.body.email});
+        if(!user) return res.status(401).send("Unauthorized!");
+
+        const vailedPassword = req.body.password === user.password;
+        if(!vailedPassword) return res.status(400).send('Unauthorized!');
+
+        return res.status(200).json(user);
+    }catch(err){
+        return res.status(500).json(err);
+    }
+})
 
 module.exports = router;
