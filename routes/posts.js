a2ea1fbd05b98072fc14b71047a024098ba258d8
Souru-Hishinuma <soul.engineer@outlook.jp>
2022-06-19 15:43:36 +0900

    timeline

--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const Post = require('../models/Post');
+const User = require('../models/User');
 
 //投稿を作成する
 router.post('/', async (req, res) =>{
@@ -50,5 +51,42 @@ router.get('/:id', async(req, res) => {
         return res.status(403).json(err);
     }
 });
-
+//特定の投稿にいいねを押す
+router.put('/:id/like', async(req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if(!post.likes.includes(req.body.userId)){
+            await post.updateOne({
+                $push: {
+                    likes: req.body.userId,
+                }
+            });
+            return res.status(200).json("投稿にいいねしました。");
+        }else{
+            await post.updateOne({
+                $pull: {
+                    likes: req.body.userId,
+                }
+            });
+            return res.status(403).json('投稿からいいねを外しました。');
+        }
+    } catch (err) {
+        return res.status(500).json(err);
+    }
+});
+//タイムラインの投稿を取得
+router.get('/timeline/all', async(req, res) => {
+    try {
+        const currentUser = await User.findById(req.body.userId);
+        const userPosts = await Post.find({userId: currentUser._id});
+        const friendPosts = await Promise.all(
+            currentUser.followings.map((friendId) => {
+                return Post.find({userId: friendId});
+            })
+        );
+        return res.status(200).json(userPosts.concat(...friendPosts));
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
 module.exports = router;
